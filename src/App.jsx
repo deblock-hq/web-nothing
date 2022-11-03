a3d9e092b4aa5d47fa1ff9f16e00b5497214bef8
Sam <sam@deblock.com>
2022-11-03 09:13:45 +0100

    Simple "probably nothing" page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Logo from './assets/deblockLogo.png';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='container'>
+        <img src={Logo} atl='Logo Deblock' />
+        <p>Probably nothing...</p>
+      </div>
+    </div>
+  )
+};
+
+export default App;
