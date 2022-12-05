123ae1edd7875d6c832e64509e4d8acc60772537
Sam <sam@deblock.com>
2022-12-05 18:00:12 +0100

    Change animation for capsules

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,8 @@ import LandingPageEn from "./components/en/LandingPage/index";
 import CountrySelection from "./components/CountrySelection";
 
 const App = () => {
-  const { i18n } = useTranslation();
-  const { lng } = useParams();
-
-  // let detectedLng = i18n.language.substring(0, 2);
-
-  // if (detectedLng !== "fr") detectedLng = "en";
-
-  // console.log("i18n", i18n);
-
   return (
     <Routes>
-      {/* {test()} */}
-      {/* <Route path="/" element={<Navigate to={`/${detectedLng}`} />} exact /> */}
       <Route path="/" element={<LandingPageEn />} exact />
       <Route path="/FR-:lng" element={<LandingPageFr />} caseSensitive />
       <Route path="/choose-your-country" element={<CountrySelection />} exact />
