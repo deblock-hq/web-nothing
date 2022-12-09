fa85bb528f1e607272bd0c6c112338cc1a732cd4
Sam <sam@deblock.com>
2022-12-09 12:05:40 +0100

    Generate blob

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import LandingPageFr from "./components/fr/LandingPage/index";
 import LandingPageEn from "./components/en/LandingPage/index";
 import CountrySelection from "./components/CountrySelection";
+import Blob from "./views/Blob";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPageEn />} exact />
+      <Route path="/blob" element={<Blob />}  />
       <Route path="/:lng-FR" element={<LandingPageFr />} />
       <Route path="/choose-your-country" element={<CountrySelection />} />
       <Route path="/en-GB" element={<Navigate to="/" />} />
